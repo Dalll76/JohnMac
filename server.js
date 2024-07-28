b02c227972759b19347280b6cc05362871b6562b
Dalll76 <daalvares@gmail.com>
2024-07-28 12:46:16 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,16 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Permitir CORS de qualquer origem
 app.use(cors());
 app.use(express.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+});
 
 app.post('/api/gpt', async (req, res) => {
     try {
