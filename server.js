349e5c6954d32c4b2e70f1e859422fa74ef648bc
Dalll76 <daalvares@gmail.com>
2024-07-28 00:07:52 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors()); // Isso permite todas as origens
+app.use(cors());
 app.use(express.json());
 
 app.post('/api/gpt', async (req, res) => {
